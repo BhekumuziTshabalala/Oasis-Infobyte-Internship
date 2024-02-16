86aa73798c5439a520282d91ca2356d0b45a7f42
Bhekumuzi Tshabalala <Bhekumuzitshabalala.main@gmail.com>
2024-02-16 14:17:17 +0200

    added button for mode

--- Task 2/Number_Guessing_Game_JFrame/src/main/java/org/bhekumuzi/myFrame.java
+++ Task 2/Number_Guessing_Game_JFrame/src/main/java/org/bhekumuzi/myFrame.java
@@ -5,13 +5,27 @@ import javax.swing.*;
 public class myFrame extends JFrame {
      myFrame(){
 
-         JButton button = new JButton();
+         JButton easy = new JButton();
+         easy.setBounds(80,100,100,50);
+         easy.setFocusable(false);
+         easy.setText("easy");
+
+         JButton medium = new JButton();
+         medium.setBounds(200,100,100,50);
+         medium.setFocusable(false);
+         medium.setText("easy");
+
+         JButton hard = new JButton();
+         hard.setBounds(320,100,100,50);
+         hard.setFocusable(false);
+         hard.setText("easy");
 
          this.setVisible(true); // so we can see the frame
          this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
          this.setResizable(false);
          this.setSize(500,500);
          this.setTitle("Number Guessing Game");
+         this.setLayout(null);
 
          JLabel label = new JLabel();
          label.setHorizontalTextPosition(JLabel.CENTER);
@@ -20,6 +34,9 @@ public class myFrame extends JFrame {
          label.setText("Ready ? Choose difficult :");
 
          this.add(label);
+         this.add(medium);
+         this.add(easy);
+         this.add(hard);
      }
 
 
